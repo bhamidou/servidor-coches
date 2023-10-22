3e299cee1fcd54ad97fc680e7b757fbaf14463a4
badr <badrhamidou@gmail.com>
2023-10-22 21:07:52 +0200

    set functions index, store and get element

--- app/Http/Controllers/Persona.php
+++ app/Http/Controllers/Persona.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Personas;
 
 class Persona extends Controller
 {
@@ -11,15 +12,23 @@ class Persona extends Controller
      */
     public function index()
     {
-        //
+        $persona = Personas::all();
+        return response()->json($persona);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $req)
     {
-        //
+        $dni = strval($req->get("DNI"));
+        $nombre = strval($req->get("Nombre"));
+        $tfno = strval($req->get("Tfno"));
+        $edad = strval($req->get("edad"));
+
+
+        \DB::insert('insert into personas (DNI, Nombre, Tfno, edad) values (?, ?, ?, ?)', [$dni, $nombre ,$tfno , $edad ]);
+        return response()->json($req);
     }
 
     /**
@@ -27,7 +36,8 @@ class Persona extends Controller
      */
     public function show(string $id)
     {
-        //
+        $findPersona = \DB::select('select * from personas where DNI = ?', [$id]);
+        return response()->json($findPersona);
     }
 
     /**
