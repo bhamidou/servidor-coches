2373421c404513cb57aabdce170ebc2f39149827
badr <badrhamidou@gmail.com>
2023-10-22 20:24:04 +0200

    create model Coche

---
+++ app/Models/Coche.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Coche extends Model
+{
+    use HasFactory;
+
+    protected $table = "coches";
+    protected $primaryKey = "Matricula";
+    public $incrementing = false;
+}
